e9ff4964712b9c49a6ebb24f80888931f6d926f7
Admin <admin@makhzan.com>
2026-05-18 23:55:00 +0300

    Deploy to Netlify - switch to npm build

---
+++ makhzan-muhadaratna/.netlify/functions-internal/___netlify-server-handler/.next/server/chunks/ssr/_next-internal_server_app_muhadara_[id]_page_actions_0kf1dft.js
@@ -0,0 +1,3 @@
+module.exports=[89737,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_muhadara_%5Bid%5D_page_actions_0kf1dft.js.map
---
+++ makhzan-muhadaratna/.netlify/functions-internal/___netlify-server-handler/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ks7Da+AnXubiZnOocxYlOqsoahhtNXlm4Dmu/wIr854=\"\n}"
